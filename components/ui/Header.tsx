1ba8598c961052375cec06514c3a3b1860dff1b3
sangyoon-kim1804 <cracklove@barunson.co.kr>
2023-05-03 18:33:30 +0900

    commit

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -8,23 +8,27 @@ import {useRouter} from 'next/router';
 export default function Home() {
   const router = useRouter();
   const [open, setOpen] = useState(false);
-  const menuOpen = () => {
-    setOpen(!open)
+  const menuToggle = () => {
+    setOpen(!open);
+    document.body.classList.remove('open');
+    if (!open) {
+      document.body.classList.add('open');      
+    }
   }
   return (
     <>
       <nav className="" style={{ zIndex: "2" }}>
-        <img src="/images/logo-renual.svg" alt="logo" className="logo" style={{ zIndex: "3" }} />
+        <object data="/images/logo-renual.svg" className="logo" style={{ zIndex: "3" }} />
         <ul className={open?"open":""}>
           {header_menus.map((item:any, index:number)=>(
           <li key={index}>
-            <Link className={item.path==router.pathname?'active':''} target={item.target} href={item.path} onClick={()=>setOpen(false)}>
+            <Link className={item.path==router.pathname?'active':''} target={item.target} href={item.path} onClick={menuToggle}>
               {item.title}
             </Link>
           </li>
           ))}
         </ul>
-        <Icon iconName={open?"XLg":"List"} onClick={menuOpen} size="24" className="d-block d-lg-none position-relative" style={{ zIndex: "3", cursor: "pointer", top: "14px" }} />
+        <Icon iconName={open?"XLg":"List"} onClick={menuToggle} size="24" className="d-block d-lg-none position-relative" style={{ zIndex: "3", cursor: "pointer", top: "14px" }} />
       </nav>
     </>
   )
