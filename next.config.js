f10c597594754bdcd2bd81f8654eb7a8df7b8356
sangyoon-kim1804 <cracklove@barunson.co.kr>
2023-05-04 16:13:41 +0900

    giveup

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== "production";
-const repository = "labs-renewal";
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
-  //basePath: "/labs-renewal",
 }
 
 module.exports = nextConfig
