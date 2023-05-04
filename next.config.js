eec997b33ad58d7b28f8986f77176f5c4a0f8f99
sangyoon-kim1804 <cracklove@barunson.co.kr>
2023-05-04 15:39:22 +0900

    images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+const repository = "http://cracklover1804.github.io/labs-renewal";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: "/labs-renewal",
+  assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  //basePath: "/labs-renewal",
 }
 
 module.exports = nextConfig
