b2c134904558d5a0834c6954ad61ba2a0b2b83d4
sangyoon-kim1804 <cracklove@barunson.co.kr>
2023-05-04 16:00:24 +0900

    deploy to gh-pages

--- components/function/Counter.tsx
+++ components/function/Counter.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Icon } from '#/components/function/Icon';
 import Col from 'react-bootstrap/Col';
-import CountUp from 'react-countup';
+//import CountUp from 'react-countup';
+import { CountUp } from 'use-count-up'
 
 export const Counter = ({ ...props }: any) => {  
   return (
@@ -10,7 +11,7 @@ export const Counter = ({ ...props }: any) => {
     <Col lg={props.size} key={index} className="mb-5 d-flex align-items-center fd-col">
       <span className="count w-100 d-flex align-items-center justify-content-lg-start jusfify-content-md-center">
         <Icon iconName={item.icon} className="me-3" size="20" /> 
-        <CountUp end={item.amount} />
+        <CountUp isCounting end={item.amount} duration={3.2} formatter={(value) => value.toLocaleString()} />
       </span>
       <small className="w-100 d-flex justify-content-lg-start jusfify-content-md-center"><b>{item.caption}</b></small>
     </Col>
