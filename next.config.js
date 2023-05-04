9bb7e17d47b2c60e115cac86e564946952ce00f6
sangyoon-kim1804 <cracklove@barunson.co.kr>
2023-05-04 16:43:21 +0900

    config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: "/labs-renewal",
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://sangyoon-kim1804.github.io/labs-renewal"
+      : "",  
 }
 
 module.exports = nextConfig
