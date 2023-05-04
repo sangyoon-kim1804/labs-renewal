ad9911b4853af750ddfb6573bd13f03da3d6a44e
sangyoon-kim1804 <cracklove@barunson.co.kr>
2023-05-04 15:10:08 +0900

    counter delete

--- components/function/Counter.tsx
+++ components/function/Counter.tsx
@@ -9,7 +9,8 @@ export const Counter = ({ ...props }: any) => {
     { props.data.map((item:any, index:number)=>(
     <Col lg={props.size} key={index} className="mb-5 d-flex align-items-center fd-col">
       <span className="count w-100 d-flex align-items-center justify-content-lg-start jusfify-content-md-center">
-        <Icon iconName={item.icon} className="me-3" size="20" /> <CountUp end={item.amount} />
+        <Icon iconName={item.icon} className="me-3" size="20" /> 
+        {/* <CountUp end={item.amount} /> */}
       </span>
       <small className="w-100 d-flex justify-content-lg-start jusfify-content-md-center"><b>{item.caption}</b></small>
     </Col>
