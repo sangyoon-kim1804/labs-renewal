67e5004635466c0e033428a714725cd4d516b750
sangyoon-kim1804 <cracklove@barunson.co.kr>
2023-05-04 16:03:03 +0900

    deploy

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   swcMinify: true,
   assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
-  basePath: "/labs-renewal",
+  //basePath: "/labs-renewal",
 }
 
 module.exports = nextConfig
